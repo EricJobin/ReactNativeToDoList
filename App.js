5e1c5ccd24df147d050496d77dcdcc46bcb8d4f2
unknown <ejobin@gmail.com>
2020-03-01 15:13:47 -0500

    added flexbox properties

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Alert, TouchableWithoutFeedback, Keyboard } from 'react-native';
 import Header from './components/header'
 import Todoitem from './components/todoitem'
 import AddToDo from './components/addtodo'
@@ -19,33 +19,43 @@ export default function App() {
   }
 
   const submitHandler = (text) =>{
-    setTodos( (prevTodos)=>{
-      return [
-        {text: text, key: Math.random().toString() },
-        ...prevTodos
-      ] //Note, we are using square brackets after the return here because what we are returning is an array, with our prvious items and the new object we are adding
-    })
+    if (text.length > 2){
+      setTodos( (prevTodos)=>{
+        return [
+          {text: text, key: Math.random().toString() },
+          ...prevTodos
+        ] //Note, we are using square brackets after the return here because what we are returning is an array, with our prvious items and the new object we are adding
+      });
+      // changeHandler('')
+    } else {
+      Alert.alert('Input Too Short', 'Tasks must be at least 3 characters in length', [{text:'Dismiss'}])
+    }
   }
 
   return (
-    <View style={styles.container}>
-      <Header />
-      <View style={styles.content}>
-        <View style={styles.list}>
-          <FlatList 
-            data = {todos}
-            renderItem = {( {item} ) => (
-              // <Text>{item.text}</Text>
-              <Todoitem item={item} pressHandler={pressHandler} />
-            )}
-          />
+    <TouchableWithoutFeedback
+      onPress={()=>{Keyboard.dismiss()}}
+    >
+      <View style={styles.container}>
+        <Header />
+
+        <View style={styles.content}>
+
+          <View style={styles.list}>
+            <FlatList 
+              data = {todos}
+              renderItem = {( {item} ) => (
+                // <Text>{item.text}</Text>
+                <Todoitem item={item} pressHandler={pressHandler} />
+              )}
+            />
+          </View>
+          <View style={styles.add}>
+            <AddToDo submitHandler={submitHandler} />
+          </View>
         </View>
-        <AddToDo submitHandler={submitHandler}/>
       </View>
-      
-
-
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -57,9 +67,21 @@ const styles = StyleSheet.create({
     // justifyContent: 'center',
   },
   content: {
+    flex:1,
     padding: 40,
+    // backgroundColor: 'gold',
   },
   list: {
+    flex:2,
     marginTop: 20,
+    // backgroundColor: 'yellow',
   },
+  add: {
+    borderTopWidth: 3,
+    borderColor: '#888',
+    // flex:1,
+    justifyContent: 'flex-end',
+    // backgroundColor: 'blue',
+    // marginTop: 50,
+  }
 });
--- components/addtodo.js
+++ components/addtodo.js
@@ -9,16 +9,26 @@ export default function AddToDo({submitHandler}) {
     setText(val);
   }
 
+  // *********************************
+  // Added this code to try and clear TextInput on Submit
+  // Text seems to clear in State (if you hit 'add task' again it says it's too short), but text is still showing on screen.
+  // Come back to this later, add this function in on button
+  // const addNewTodo = (text) =>{
+  //   submitHandler(text)
+  //   setText('')
+  // }
+  //*******************************/
 
   return (
     <View>
       <TextInput
         style={styles.input}
-        placeholder='New Task'
+        placeholder=' New Task'
         onChangeText={changeHandler}
       />
       <Button
         onPress={()=>submitHandler(text)}
+        // onPress={()=>addNewTodo(text)}
         title='Add Task'
         color='coral'
       />
@@ -29,15 +39,21 @@ export default function AddToDo({submitHandler}) {
 const styles = StyleSheet.create({
   input: {
     marginTop: 10,
-    paddingHorizontal: 8,
-    paddingVertical: 6,
-    borderBottomWidth: 1,
-    borderBottomColor: '#ddd',
+    marginBottom: 5,
+    // paddingHorizontal: 8,
+    // paddingVertical: 6,
+    borderWidth: 1,
+    borderColor: '#555',
+    // borderBottomWidth: 2,
+    // borderBottomColor: '#ddd',
+    // borderTopColor: '#eee',
+
+    // borderBottomColor: 'red',
     // paddingTop: 16,
     // marginTop: 16,
     // borderColor: '#bbb',
     // borderWidth: 1,
     // borderStyle:  'dashed',
-    // borderRadius: 10,
+    borderRadius: 10,
   },
 });
